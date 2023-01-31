e6e8456a6acebd388e955266731c064ec3a3b8ec
pavl_g <60224159+Scrappers-glitch@users.noreply.github.com>
2023-01-31 18:15:47 +0200

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
 
-int main(int argc, char **argv)
-{
-	printf("give me a bottle of rum!\n");
-	return 0;
+int main() {
+	
+	printf("%d\n", sizeof(long));
+	printf("%d\n", sizeof(long int));
+	printf("%d\n", sizeof(long long));
+    
+    return 0;
 }
