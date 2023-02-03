f9e747873e45aa37ff7a37484d3e4af75f0ba106
pavl_g <60224159+Scrappers-glitch@users.noreply.github.com>
2023-02-03 23:18:39 +0200

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,11 +1,9 @@
-// Online C compiler to run C program online
-#include <stdio.h>
+#include<stdio.h>
 #include<unistd.h>
+#include<stdlib.h>
 
 int main() {
-    // Write C code here
     const char* buffer = (char*) calloc(1, sizeof(char));
     printf("%s\n", getcwd(buffer, sizeof(buffer)));
-
     return 0;
 }
