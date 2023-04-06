f5639c7fbe644396915e720f081730d80ac30b7b
pavl_g <60224159+Scrappers-glitch@users.noreply.github.com>
2023-04-06 10:03:29 +0200

    Update hello-bash.sh

--- hello-bash.sh
+++ hello-bash.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-if [[ `uname` == "Linux" ]]; then
-   lib="libtest.so"
-elif [[ `uname` == "Darwin" ]]; then
-   lib="libtest.dylb"
-else 
-   lib="libtest.dll"
-fi   
+uname
 
-gcc -pie hello.c -o $lib
-
-./$lib
-
-mvn gpg:sign-and-deploy-file --version
 
